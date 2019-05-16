3776ae05847cc7a440a41a2953058d4db634016a
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-05-16 21:28:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,11 +3,13 @@ def new_hash
 end
 
 def actor
-	actor = {:name => "Dwayne The Rock Johnson"}
+	actor = {name: "Dwayne The Rock Johnson"}
 end
 
 def monopoly
-	monopoly = {}
+	monopoly = {
+		:railraods => {}
+	}
 
 end
 
